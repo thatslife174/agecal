08d90003c25a6eb25367c7126b381acf23ab61aa
thatslife174 <114251369+thatslife174@users.noreply.github.com>
2022-10-29 09:55:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function get_result(){
     var _age = document.getElementById("age").value
     var res = document.getElementById("result")
     if(_age == ''){
-        res.innerHTML = "Bạn đã nhập đéo đâu"
+        res.innerHTML = "Bạn đã nhập đéo đâu."
     } else if(_age > 0 && _age <= 1000){
-        res.innerHTML = "Bạn năm nay " + _age + " tuổi"
+        res.innerHTML = "Bạn năm nay " + _age + " tuổi."
     } else {
-        res.innerHTML = _age + " là tuổi clg"
+        res.innerHTML = _age + " là tuổi clg."
     }
 }
